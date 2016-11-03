ff387ea5bda7730dd4cf1428f637c82de537b381
Prokopev Alexander <a.prokopev@qiwi.ru>
2016-11-04 01:30:32 +0300

    refactoring

--- app/src/Finances/src/Services/Transactions/ITransactionStore.cs
+++ app/src/Finances/src/Services/Transactions/ITransactionStore.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Finances.Data;
-using Finances.Models;
-using Finances.Services.Books;
-using Finances.Services.Users;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
+﻿using System.Threading.Tasks;
 
 namespace Finances.Services.Transactions {
     public interface ITransactionStore {
